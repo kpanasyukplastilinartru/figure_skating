45ce49a3fc84c08bce9122185c0d8a22de1953dd
Soveliy <nikita.elagin2012@yandex.ru>
2022-08-02 14:35:17 +0300

    added cookie

---
+++ src/scripts/blocks/preloader.js
@@ -0,0 +1,64 @@
+function preloader(e){
+    $('#marquee').marquee({
+        duration: 25000,
+        startVisible: true,
+        duplicated: true,
+        pauseOnHover:false,
+    });
+   
+    let cookieOptions = { expires: 7, path: '/' };
+    e.cookie('visit', false, cookieOptions);
+    if (e.cookie('visit') == undefined) {
+        setTimeout(() => {
+            $( ".preloader__image-container--1" ).addClass("animated")
+        }, 1000);
+        $( ".preloader__image-container--1" ).addClass("animated")
+        setTimeout(() => {
+        $( ".preloader__image-container--2" ).addClass("animated")
+        }, 700);
+        setTimeout(() => {
+            $( ".preloader__image-container--2" ).addClass("scale")
+         }, 1300);
+         setTimeout(() => {
+            $( ".preloader__image-container--2" ).removeClass("animated").removeClass("scale")
+            $( ".preloader__image-container--3" ).addClass("animated")
+            $( ".preloader__image-container--4" ).addClass("animated")
+         }, 2100);
+         setTimeout(() => {
+            $( ".preloader__image-container--3" ).removeClass("animated")
+            $( ".preloader__image-container--4" ).removeClass("animated")
+        }, 3300);
+        setTimeout(() => {
+            $( ".preloader__image-container--5" ).addClass("animated")
+        }, 3500);
+        setTimeout(() => {
+            $( ".preloader__image-container--5" ).addClass("end-animated")
+        }, 4500);
+        setTimeout(() => {
+            $(".preloader").fadeOut();
+        }, 5100);
+            e.cookie('visit', true, cookieOptions);
+           
+    } else {
+        $(".preloader").fadeOut();
+    }
+
+    // if (Cookies.get('preloader')){
+    //     $(".preloader").fadeOut();
+    // } else {
+        
+    //     Cookies.set('preloader', 'true', { expires: 7 });
+
+    // }
+  
+  
+}
+$(document).ready(function() { 
+    if($.cookie("popupnotify") == null) {
+        console.log("cookie")
+      $.cookie("popupnotify", "2");
+     } 
+    });
+$(document).ready(function(){
+    preloader()
+})
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,55 @@
 // $('body').on('mouseenter', '.ymaps-2-1-79-placemark-overlay', function() {
 //    console.log("hover")
 // });
+$(document).ready(function(){
+    $(".direction-item").click(function(){
+        $("#prezidium").arcticmodal();
+    })
+    $(".blanks__item").click(function(e){
+        e.preventDefault();
+        $("#report").arcticmodal();
+    })
+    $(".select").niceSelect();
+    $("form").each(function(  ) {
+        $(this).validate({
+           
+             messages:{
+               name:{
+                 required: "Обязательно для заполнения",
+               
+             },
+               date:{
+               required: "Обязательно для заполнения",
+               },
+               coment:{
+                required: "Обязательно для заполнения",
+                },
+                discipline:{
+                required: "Обязательно для заполнения",
+                },
+             }
+          });
+      });
+      autosize($('textarea'));
+    new SimpleBar($('.prezidium-modal__content-block.content-block')[0]);
+    // $('.report-form input,.report-form textarea').on('keyup', function() {
+    //     console.log("qq")
+    //     let empty = false;
+    //     let emptyTx = false;
+    //     $('.report-form input').each(function() {
+    //       empty = $(this).val().length == 0;
+    //     });
+    //     $('.report-form textarea').each(function() {
+    //         emptyTx = $(this).val().length == 0;
+    //       });
+       
+    
+    //     if (empty && emptyTx)
+    //       $('.report-form .form__button ').attr('disabled', 'disabled');
+    //     else
+    //       $('.report-form .form__button ').attr('disabled', false);
+    //   });
+
+      
+})
+    
